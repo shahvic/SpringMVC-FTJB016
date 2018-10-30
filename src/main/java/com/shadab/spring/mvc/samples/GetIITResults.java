f09dd6bfc1b3e24a0a7b218cc50d31e13a0b6a92
Mohammad.Shadab <mohammad.shadab@192.168.1.2>
2018-10-30 21:34:48 +0530

    changes

---
+++ src/main/java/com/shadab/spring/mvc/samples/GetIITResults.java
@@ -0,0 +1,36 @@
+package com.shadab.spring.mvc.samples;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class GetIITResults extends HttpServlet{
+	// roll number
+	// result
+	public void doTask() {}
+	int a=10;
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("Hello servlet is getting executed!!!!");
+		String rollNumber = req.getParameter("rollnumber");
+		PrintWriter pw = resp.getWriter();
+		pw.write("<html><body>Hello i am old servlet result for roll number  "+rollNumber+" 60%");
+				
+	 
+		pw.write("</body></html>");
+	
+	}
+	
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("do Post being executed!!!!");
+	}
+
+}
